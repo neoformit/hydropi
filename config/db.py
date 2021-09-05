fc56217f9b19ae45c8a73b6efabf1fa32abe90de
Cameron Hyde <c.hyde@qcif.edu.au>
2021-09-05 14:29:57 +1000

    Update requirements

--- config/db.py
+++ config/db.py
@@ -1,7 +1,6 @@
 """Manage optional database interface for dynamic app configuration."""
 
 import logging
-# import sqlalchemy
 
 logger = logging.getLogger(__name__)
 
@@ -12,7 +11,7 @@ class DB:
     def __init__(self):
         """Initiate database connection."""
         self.connected = False
-        pass
+        self.connect()
 
     def get(self, key):
         """Return value for given field."""
@@ -20,3 +19,11 @@ class DB:
             return "the value"
 
         logger.warning("DB.get(): could not connect to database")
+
+    def connect():
+        """Create connection to the database.
+
+        Do we want to use SQLAlchemy or just SQLite3?
+        """
+        # config.DATABASE.SQLITE_PATH
+        # config.DATABASE.TABLE_NAME
